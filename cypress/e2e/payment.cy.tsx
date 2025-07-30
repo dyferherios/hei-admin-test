7cf6aefcf3ed8d3d3ed98550f3663e14444035c1
Clermont Judicaël <135115381+ClermontJudicael@users.noreply.github.com>
2025-07-30 14:22:45 +0300

    feat: add cleanup after test finishes
    Working on improving the code

--- cypress/e2e/payment.cy.tsx
+++ cypress/e2e/payment.cy.tsx
@@ -2,79 +2,72 @@ import { loginAs } from "../script/utils";
 
 
 describe("Login Casdoor", () => {
-  it("Connect as student, navigate to Profil and Fees, and test merchant code copy", () => {
+
+  it("Log in as a manager, navigate to fees list and add one", () => {
+    loginAs("MANAGER");
+    cy.contains("Étudiants").click();
+    cy.contains("Liste des étudiants").click();
+    cy.url().should('match', /students$/);
+
+    cy.get('[data-testid="main-search-filter"]').type('ryan');
+
+    cy.contains('Ryan', { timeout: 4000 }).click();
+
+    cy.get('[data-testid="fees-tab"]').click();
+    cy.get('[data-testid="MoreVertIcon"]').click();
+    cy.get('[data-testid="create-button"]').click();
+    cy.get('#predefinedType').click();
+
+    cy.contains('li', 'Frais annuel L1').click();
+    cy.contains("Date limite à chaque fin du mois ?").click();
+    cy.get('#due_datetime').type('2222-12-12');
+    cy.contains("Enregistrer").click();
+  });
+
+  it("Log in as Student and confirm the added fee", () => {
     loginAs("STUDENT");
 
     cy.contains('Frais').click();
-
     cy.url().should('include', '/students/student1_id/fees');
 
-    cy.get('.MuiBox-root.css-w2h2dk').should('be.visible');
-    cy.get('.MuiBox-root.css-w2h2dk .MuiBox-root.css-erema2').should('have.length', 6);
-    cy.get('.MuiBox-root.css-w2h2dk .MuiBox-root.css-erema2').eq(0).should('contain.text', '3');
-    cy.get('.MuiBox-root.css-w2h2dk .MuiBox-root.css-erema2').eq(1).should('contain.text', '1');
-    cy.get('.MuiBox-root.css-w2h2dk .MuiBox-root.css-erema2').eq(2).should('contain.text', '7');
-    cy.get('.MuiBox-root.css-w2h2dk .MuiBox-root.css-erema2').eq(3).should('contain.text', '6');
-    cy.get('.MuiBox-root.css-w2h2dk .MuiBox-root.css-erema2').eq(4).should('contain.text', '1');
-    cy.get('.MuiBox-root.css-w2h2dk .MuiBox-root.css-erema2').eq(5).should('contain.text', '0');
-    cy.get('.MuiBox-root.css-w2h2dk').click();
-    cy.contains('Code marchand copié!').should('be.visible');
-    cy.window().then((win) => {
-      win.navigator.clipboard.readText().then((text) => {
-        expect(text).to.equal('317610');
-      });
-    });
-    cy.contains('Code marchand copié!', { timeout: 5000 }).should('not.exist');
+    cy.contains('span.MuiTypography-body2', '12 décembre 2222')
+      .parents('tr.MuiTableRow-root')
+      .find('svg[data-testid^="addMobileMoney-student1_id"]')
+      .click();
 
-    cy.get('[data-testid^="showButton-"]').first().click();
+    cy.get('.MuiDialog-container').should('be.visible');
 
-    cy.url().should('include', '/fees/');
-    cy.url().should('include', '/show');
+    // 1. Input "MP240731.1518.B11223" into the "Référence de la transaction" field
+    cy.contains('label', 'Référence de la transaction')
+      .next('div')
+      .find('input')
+      .type('MP240731.1518.B11223');
 
-    cy.contains('Détails du frais').should('be.visible');
-    cy.contains('Informations sur le frais').should('be.visible');
-    cy.contains('Dates importantes').should('be.visible');
-
-    cy.contains('Total à payer').should('be.visible');
-    cy.contains('Reste à payer').should('be.visible');
-    cy.contains('Date limite de paiement du frais').should('be.visible');
-    cy.contains('Date de création').should('be.visible');
-    cy.contains('Statut').should('be.visible');
-
-    cy.contains('Paiements').should('be.visible');
-    cy.get('.MuiTable-root tbody tr').should('have.length.greaterThan', 0);
-
-    cy.get('[data-testid="download-button"]').first().click();
-
-    cy.contains('Reçu en cours de téléchargement').should('be.visible');
-
+    cy.contains('button', 'Enregistrer')
+      .should('be.enabled')
+      .click();
   });
 
-  it("Connect as a manager, navigate to fees and check details", () => {
+  // cleanup
+  it("Login back as Manager and delete previously created fee", () => {
     loginAs("MANAGER");
 
     cy.contains("Étudiants").click();
-    cy.contains("Frais (en retard par défaut)").click();
-    cy.url().should('match', /fees$/);  
+    cy.contains("Liste des étudiants").click();
+    cy.url().should('match', /students$/);
 
-    cy.get('a[aria-label="Afficher"]').first().click();
-    cy.contains("Créer").click();
-    cy.url().should('match', /create$/);
+    cy.get('[data-testid="main-search-filter"]').type('ryan');
 
-    cy.contains("Mobile money").click();
-    cy.contains("Réference de la transaction").should('be.visible');
+    cy.contains('Ryan', { timeout: 4000 }).click();
 
-    cy.contains("Cash").click();
+    cy.get('[data-testid="fees-tab"]').click();
+    cy.contains("2222").click();
 
-    cy.contains("Enregistrer").click();
-    cy.contains('Le formulaire n\'est pas valide', { timeout: 5000 }).should('not.exist');
+    cy.get('[data-testid="delete-button-confirm"]').click();
 
-    cy.contains("Frais prédéfinis").click();
-    cy.url().should('match', /fees-templates$/);
-
-    cy.get('input[data-testid="main-search-filter"]')
-      .type("validation-test");
-    // "Taille" should be 1 cause only one matches the search
-    cy.contains("Taille: 1", { timeout: 4000 }).should('be.visible');
+    cy.contains('button', 'Supprimer')
+      .should('be.visible')
+      .click();
   });
+
 });
