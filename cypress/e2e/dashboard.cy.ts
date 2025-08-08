4f649c793ed720cb37b067069f96556818929228
tsilavina007 <andriamiart114@gmail.com>
2025-08-08 16:49:38 +0300

    refactor: add comment at the dashboard test

--- cypress/e2e/dashboard.cy.ts
+++ cypress/e2e/dashboard.cy.ts
@@ -2,20 +2,25 @@ import { loginAs } from "../script/utils";
 
 describe("Dashboard test", () => {
   it("Teacher dashboard is visible", () => {
+	// Connect as a teacher
 	loginAs("TEACHER");
 
+	// Vérifier que les détails du profil est visible
 	cy.contains("Dashboard").should("be.visible").click();
 	cy.contains("Détails du Profil").should("be.visible").click();
 	cy.contains("Coordonnées").should("be.visible");
-	
+
+	// Vérifier que la boîte aux lettres est visible
 	cy.contains("Dashboard").should("be.visible").click();
 	cy.contains("Boîte aux lettres").should("be.visible").click();
 	cy.get('[data-testid="letter-create-button"]').should("be.visible");
   });
 
   it("Manager dashboard is visible", () => {
+	// Connect as a manager
 	loginAs("MANAGER");
 
+	// Vérifier que les lettres, les frais et les commentaires sont visibles
 	cy.contains("Dashboard").should("be.visible").click();
 	cy.contains("Lettres récemment envoyées").should("be.visible");
 	cy.contains("Listes des frais en retard").should("be.visible");
@@ -23,17 +28,21 @@ describe("Dashboard test", () => {
   });
 
   it("Student dashboard is visible", () => {
+	// Connect as a student
 	loginAs("STUDENT");
 
+	// Vérifier que les informations personnelles est visible
 	cy.contains("Dashboard").should("be.visible").click();
 	cy.get('[data-testid="profile-tab"]').should("be.visible").click();
 	cy.contains("Coordonnées").should("be.visible");
 	cy.contains("Détails personnels").should("be.visible");
 	cy.contains("Informations personnelles").should("be.visible");
 
+	// Vérifier que les commentaires est visible
 	cy.contains("Dashboard").should("be.visible").click();
 	cy.get('[data-testid="comments-tab"]').should("be.visible").click();
 
+	// Vérifier que les lettres est visible
 	cy.contains("Dashboard").should("be.visible").click();
 	cy.get('[data-testid="user-letters-tab"]').should("be.visible").click();
 	cy.get('[data-testid="letter-create-button"]').should("be.visible").click();
