c85dc6840f8d030bb76da11c9235cb3abf14a339
tsilavina007 <andriamiart114@gmail.com>
2025-08-20 13:34:50 +0300

    test: all test down after down

--- cypress/script/main.ts
+++ cypress/script/main.ts
@@ -6,51 +6,6 @@ import { runCypressTests } from "./componentStatus/runCypressTestAndUpdateStatus
 import { determineStatus } from "./componentStatus/determineStatus";
 import { handleIncident } from "./incidents/handleIncident";
 
-// export const main = async () => {
-//   const __filename = fileURLToPath(import.meta.url);
-//   const __dirname = path.dirname(__filename);
-//   const rootPath = path.resolve(__dirname, "..");
-
-//   const componentIds = getComponentIds();
-
-//   const statusPageId = process.env.STATUS_PAGE_ID;
-//   const apiKey = process.env.API_KEY;
-
-//   const files = await fg("e2e/*.cy.ts", { cwd: rootPath });
-
-//   let hasFailures = false;
-  
-
-//   for (const file of files) {
-//     const filename = path.basename(file, ".cy.ts");
-//     const componentId = componentIds[filename.toLowerCase()];
-
-//     if (!componentId) {
-//       console.warn("no component found")
-//     }
-
-//     const { failedTests, totalTests } = await runCypressTests(
-//       componentId,
-//       statusPageId!,
-//       apiKey!,
-//       file
-//     );
-
-//     const status = await determineStatus(failedTests, totalTests);
-
-//     if (failedTests > 0) {
-//       hasFailures = true;
-//       console.warn(`test failed with file ${file}`);
-//     } else {
-//       console.log(`test passed with file ${file}`);
-//     }
-//   }
-
-//   process.exit(hasFailures ? 1 : 0);
-// };
-
-// main();
-
 export const main = async () => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
@@ -97,7 +52,7 @@ export const main = async () => {
     }
   }
 
-  process.exit(hasFailures ? 1 : 0);
+  // process.exit(hasFailures ? 1 : 0);
 };
 
 main();
